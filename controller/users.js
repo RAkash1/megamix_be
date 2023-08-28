2c3fcacd3014101aed970edd4454083253dd52f5
Akash Raturi <akashraturi1234@gmail.com>
2023-08-28 20:28:57 +0530

    final commit

--- controller/users.js
+++ controller/users.js
@@ -1,146 +1,165 @@
-const bcrypt = require('bcrypt');
-const Users = require('../models/users')
-const Post = require('../models/posts')
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
-const { Console } = require('console');
+const bcrypt = require("bcrypt");
+const Users = require("../models/users");
+const Post = require("../models/posts");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
-const registerUser=async(req,res)=>{
-    const {name,email,password}= req.body;
-    console.log(name,email,password);
-    const hashpass=await bcrypt.hash(password,10);
-    console.log(hashpass);
-    try{
-    await Users.create({ 
-        username:name,
-        email,
-        password:hashpass
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  // console.log(name, email, password);
+  const hashpass = await bcrypt.hash(password, 10);
+  // console.log(hashpass);
+  try {
+    await Users.create({
+      username: name,
+      email,
+      password: hashpass,
     });
-    res.status(201).json({message:'user created'})
-    }
-    catch(e){
-        res.status(400).json(e)
-    }
-}
+    res.status(201).json({ message: "user created" });
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
 
-
-const loginUser=async(req,res)=>{
-    const{email,password}=req.body;
-    console.log(email,password);
-    const userFind = await Users.findOne({email});
-    console.log(userFind);
-    if(userFind){
-        const match = await bcrypt.compare(password,userFind.password);
-        if(match){
-        jwt.sign({username:userFind.username ,email,id:userFind._id},'secretkey',(err,token)=>{
-            res.cookie('token',token).json({username:userFind.username,id:userFind._id})
-            console.log(username)
-        }) 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  // console.log(email, password);
+  const userFind = await Users.findOne({ email });
+  // console.log(userFind);
+  if (userFind) {
+    const match = await bcrypt.compare(password, userFind.password);
+    if (match) {
+      jwt.sign(
+        { username: userFind.username, email, id: userFind._id },
+        "secretkey",
+        (err, token) => {
+          res
+            .cookie("token", token)
+            .json({ username: userFind.username, id: userFind._id });
+          // console.log(username);
         }
-        else{
-            res.status(400).json({message:'user password not match'})
-        }
+      );
+    } else {
+      res.status(400).json({ message: "user password not match" });
     }
-    else{
-        res.status(404).json({message:'user not found'})
-    }
-}
-const logoutUser=(req,res)=>{
-    res.cookie('token', '').json('ok');
-}
-const getUser=(req,res)=>{
-    const {token} = req.cookies
-    jwt.verify(token,'secretkey',(err,info)=>{
-        if(err) res.status(400).json(err)
-        res.json(info)})
-}
+  } else {
+    res.status(404).json({ message: "user not found" });
+  }
+};
 
-const createpost =async (req,res) => {
-    console.log(req.file);
+const logoutUser = (req, res) => {
+  res.cookie("token", "").json("ok");
+};
 
-    const{originalname,path}=req.file;
-    const parts=originalname.split('.');
-    // const newPath=path+'.'+parts[parts.length - 1];
-    const ext = parts[parts.length - 1];
-    newPath = path+'.'+ext;
-    fs.renameSync(path, newPath);
-    console.log(parts);
-    const {token} = req.cookies;
-    jwt.verify(token, 'secretkey', {}, async (err,info) => {
+const getUser = (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, "secretkey", (err, info) => {
+    if (err) res.status(400).json(err);
+    res.json(info);
+  });
+};
+
+const createpost = async (req, res) => {
+  // console.log(req.file);
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+  console.log(parts);
+  const { token } = req.cookies;
+  jwt.verify(token, "secretkey", {}, async (err, info) => {
     console.log(info);
     if (err) throw err;
-    const {title,summary,content} = req.body;
+    const { title, summary, content } = req.body;
     const postDoc = await Post.create({
       title,
       summary,
       content,
-      cover:newPath,
-      author:info.id,
+      cover: newPath,
+      author: info.id,
     });
     res.json(postDoc);
   });
+};
 
-}
-const getPost =async (req,res) => {
-    res.json(
-      await Post.find()
-        .populate('author', ['username'])
-        .sort({createdAt: -1})
-    );
-}
-const getPostDetails =async (req,res) => {
-    console.log(req.params.id);
-    res.json( await Post.findById(req.params.id).populate('author', ['username']))
-}
-const userProfile =async (req,res) => {
-    const {token} = req.cookies;
-    jwt.verify(token, 'secretkey', {}, async (err,info) => {
-        console.log(info);
-        result= await Post.find({author:info.id}).populate('author', ['username'])
-        res.json(result);
-      });     
+const getPost = async (req, res) => {
+  console.log(req.cookies);
+  res.json(
+    await Post.find().populate("author", ["username"]).sort({ createdAt: -1 })
+  );
+};
+
+const getPostDetails = async (req, res) => {
+  console.log(req.params.id);
+  res.json(await Post.findById(req.params.id).populate("author", ["username"]));
+};
+
+const userProfile = async (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, "secretkey", {}, async (err, info) => {
+    console.log(info);
+    result = await Post.find({ author: info.id }).populate("author", [
+      "username",
+    ]);
+    res.json(result);
+  });
+};
+
+const deletePost = async (req, res) => {
+  // console.log(req.params.id);
+  const img = await Post.findById(req.params.id);
+  // console.log(img.cover);
+  fs.unlinkSync(img.cover, (err) => {
+    console.log(err);
+  });
+  // console.log(img.cover);
+  const responce = await Post.findByIdAndDelete(req.params.id);
+  // console.log(responce);
+  res.status(200).json({ message: "post deleted"});
+};
+
+const editpost = async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, "secretkey", {}, async (err, info) => {
+    if (err) throw err;
+    const id = req.params.id;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("you are not the author");
     }
-const deletePost =async (req,res) => {
-        console.log(req.params.id);
-        const img = await Post.findById(req.params.id);
-        console.log(img.cover);
-        fs.unlinkSync(img.cover, (err) => {console.log(err)});
-        console.log(img.cover);
-        const responce=await Post.findByIdAndDelete(req.params.id);
-        console.log(responce);
-        res.status(200).json(responce);
-}
-const editpost =async (req,res) => {
-    let newPath = null;
-    if (req.file) {
-        const {originalname,path} = req.file;
-        const parts = originalname.split('.');
-        const ext = parts[parts.length - 1];
-        newPath = path+'.'+ext;
-        fs.renameSync(path, newPath);
-    }
-
-    const {token} = req.cookies;
-    jwt.verify(token, 'secretkey', {}, async (err,info) => {
-        if (err) throw err;
-        const id = req.params.id;
-        const {title,summary,content} = req.body;
-        const postDoc = await Post.findById(id);
-        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-        if (!isAuthor) {
-        return res.status(400).json('you are not the author');
-        }
-        console.log(postDoc);
-        await postDoc.updateOne({
-        title,
-        summary,
-        content,
-        cover: newPath ? newPath : postDoc.cover,
-        });
-
-        res.json(postDoc);
+    console.log(postDoc);
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
     });
-}
 
+    res.json(postDoc);
+  });
+};
 
-module.exports={registerUser,loginUser,logoutUser,getUser,createpost,getPost,getPostDetails,userProfile,deletePost,editpost}
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUser,
+  createpost,
+  getPost,
+  getPostDetails,
+  userProfile,
+  deletePost,
+  editpost,
+};
--- models/users.js
+++ models/users.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const UserScheme = new mongoose.Schema({
-    username:{
-        type:String,
-        require:true
-    },
-    email:{
-        type:String,
-        require:true,
-        unique: true
-    },
-    password:{
-        type:String,
-        require:true
-    }
-})
-const User = mongoose.model('User',UserScheme)
-module.exports=User
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+const User = mongoose.model("User", UserScheme);
+module.exports = User;
