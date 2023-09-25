c4c49dfe11f5bc714395bfd990fcb257c46fa114
Akash Raturi <akashraturi1234@gmail.com>
2023-09-25 22:26:40 +0530

    .

--- connect/mongo.js
+++ connect/mongo.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
+  console.log(process.env.MONGO_URL)
   return mongoose.connect(process.env.MONGO_URL)
   .then(console.log("<-------> connection successfull <------->"))
   .catch((err) => console.log(err));
